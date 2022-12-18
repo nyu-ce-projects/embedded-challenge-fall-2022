9a461f18e428bc45e6d6bc5318feecaf6733ec34
adityat354 <adityat354@gmail.com>
2022-12-17 20:21:52 -0500

    counter,display update

--- src/main-stretchSensor.cpp
+++ src/main-stretchSensor.cpp
@@ -1,36 +1,36 @@
-#include <mbed.h>
+// #include <mbed.h>
 
-AnalogIn   ain(PA_0, 3);
-DigitalOut dout(PG_13);
-uint32_t counter=0; 
+// AnalogIn   ain(PA_0, 3);
+// DigitalOut dout(PG_13);
+// uint32_t counter=0; 
 
-int mainStretchSensor() {
+// int mainStretchSensor() {
     
-  while (1) {
-        // test the voltage on the initialized analog pin
-        //  and if greater than 0.3 * VCC set the digital pin
-        //  to a logic 1 otherwise a logic 0
-        printf("normal %f", ain.read_voltage());
-        volatile float voltage = ain.read_voltage();
-        if(voltage > 0.90) {
-            printf("no breathing\n");
-            counter++;
-            if (counter >= 10){
-              dout=1; 
-              //wait(2);
-            }
-        } else {
-            printf("breathing\n");
-            dout=0;
-            counter=0;
-        }
+//   while (1) {
+//         // test the voltage on the initialized analog pin
+//         //  and if greater than 0.3 * VCC set the digital pin
+//         //  to a logic 1 otherwise a logic 0
+//         printf("normal %f", ain.read_voltage());
+//         volatile float voltage = ain.read_voltage();
+//         if(voltage > 0.90) {
+//             printf("no breathing\n");
+//             counter++;
+//             if (counter >= 10){
+//               dout=1; 
+//               //wait(2);
+//             }
+//         } else {
+//             printf("breathing\n");
+//             dout=0;
+//             counter=0;
+//         }
 
-        //display_voltage(voltage);
+//         //display_voltage(voltage);
         
-        // print the percentage and 16 bit normalized values
-        //printf("\npercentage: %.3f\n", voltage*100.0f);
-        //printf("normalized: 0x%04X \n", ain.read_u16());
-        thread_sleep_for(1000);
-  }
-  return 0;
-}
+//         // print the percentage and 16 bit normalized values
+//         //printf("\npercentage: %.3f\n", voltage*100.0f);
+//         //printf("normalized: 0x%04X \n", ain.read_u16());
+//         thread_sleep_for(1000);
+//   }
+//   return 0;
+// }
--- src/main-ultrasonicDistanceSensor.cpp
+++ src/main-ultrasonicDistanceSensor.cpp
@@ -3,30 +3,46 @@
 
  
 DigitalOut trigger(PA_6);
+DigitalOut dout(PG_13);
 //DigitalOut myled(LED1); //monitor trigger
 //DigitalOut myled2(LED2); //monitor echo
 DigitalIn  echo(PA_7);
 int distance_new = 0;
+int distance_old = -1;
 int correction = 0;
+int delta = 0;
+int counter = 0;
+bool breathe_flag = true;
 Timer sonar;
 
 LCD_DISCO_F429ZI lcd;
-uint8_t msg[50];
+uint8_t msg1[50];
+uint8_t msg2[50];
 
-void display_voltage(int value)
+void display_status(int value)
 {
     lcd.SetTextColor(LCD_COLOR_BLACK);
     lcd.Clear(LCD_COLOR_LIGHTCYAN);
     lcd.SetBackColor(LCD_COLOR_LIGHTCYAN);
     BSP_LCD_DrawHLine(0, LINE(4), 240);
-    lcd.DisplayStringAt(0, LINE(1), (uint8_t *)"START INCREASING", CENTER_MODE);
-    lcd.DisplayStringAt(0, LINE(2), (uint8_t *)"THE PRESSURE VALUE", CENTER_MODE);
-    lcd.DisplayStringAt(0, LINE(3), (uint8_t *)"TILL IT IS 200mmHg", CENTER_MODE);
-    sprintf((char *)msg, "%d mmHg", value);
-    lcd.DisplayStringAt(0, LINE(10), (uint8_t *)&msg, CENTER_MODE);
-    BSP_LCD_DrawRect(40, LINE(8), 160, LINE(5));
+    lcd.DisplayStringAt(0, LINE(1), (uint8_t *)"Just Breathe", CENTER_MODE);
+    lcd.DisplayStringAt(0, LINE(2), (uint8_t *)"KEEP THE SENSOR", CENTER_MODE);
+    lcd.DisplayStringAt(0, LINE(3), (uint8_t *)"IN A RANGE OF ~300 MM", CENTER_MODE);
+    if(breathe_flag)
+    {
+        sprintf((char *)msg1, "|| Breathing ||");
+        dout=0;
+    }
+    else
+    {
+        sprintf((char *)msg1, "!! Not Breathing !!");
+        dout=1;
+    }
+    sprintf((char *)msg2, "Distance: %d mm", value);
+    lcd.DisplayStringAt(5, LINE(10), (uint8_t *)&msg1, CENTER_MODE);
+    lcd.DisplayStringAt(5, LINE(12), (uint8_t *)&msg2, CENTER_MODE);
+    //BSP_LCD_DrawRect(5, LINE(8), 200, LINE(5));
 }
-
 
 int main()
 {
@@ -43,20 +59,18 @@ int main()
 // read timer
     correction = sonar.read_us();
     printf("Approximate software overhead timer delay is %d uS\n\r",correction);
+
  
 //Loop to read Sonar distance values, scale, and print
     while(1) {
 // trigger sonar to send a ping
+        breathe_flag = true;
         trigger = 1;
-        //myled = 1;
-        //myled2 = 0;
         sonar.reset();
         wait_us(10.0);
         trigger = 0;
-        //myled = 0;
 //wait for echo high
         while (echo==0) {};
-        //myled2=echo;
 //echo high, so start timer
         sonar.start();
 //wait for echo low
@@ -65,10 +79,29 @@ int main()
         sonar.stop();
 //subtract software overhead timer delay and scale to cm
         distance_new = (sonar.read_us()-correction)/5.8;
-        //myled2 = 0;
         //printf(" %d cm \n\r", distance_new);
-        display_voltage(distance_new);
+        delta = abs(distance_old - distance_new);
+        distance_old = distance_new;
+        if(delta <= 3)
+        {  if(counter < 10)
+              counter++ ; //start counter
+        }
+        else
+        {
+            counter = 0; //reset counter
+        }
+        
+        if(counter >= 10)
+        {
+          breathe_flag = false;
+          display_status(distance_new);
+          break;
+        }
+        display_status(distance_new);
+
+
+        
 //wait so that any echo(s) return before sending another ping
-        wait_us(500000);
+        wait_us(1000000);
     }
 }
